c0e3fb78bf58661feb4786fc5ac7c05faffdc238
ElaySuperCoder <36122259+Maharramli@users.noreply.github.com>
2020-04-23 15:18:53 +0400

    My Blog

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import loader
-
-from .models import Question
-
+from django.urls import reverse
+from django.views import generic
+from .models import Question,Choice
 
 
 def index(request):
@@ -18,7 +18,7 @@ def index(request):
     return HttpResponse(output)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    return HttpResponse("Sənin sualının nömrəsi %s-dü" % question_id)
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
@@ -31,3 +31,18 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+
